b2541cbe4f8978c14f5c3abd70967470f9ce9a8f
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-06 21:45:21 -0500

    Created block struct

---
+++ src/block.rs
@@ -0,0 +1,18 @@
+/// # Block
+///
+/// A `Block` represents an entry in the blockchain
+///
+/// ## Synopsis
+///
+/// A block contains the following:
+/// - an index (the index of the node)
+/// - a list of transactions
+/// - proof
+/// - a hash of the previous block
+
+struct Block {
+    index: u64, // index in the blockchain
+    proof: u64,
+    prev_hash: String, // hash of the previous block
+}
+
