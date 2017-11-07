6099f199823ffc54fb3603bfef3ba48f3da1d246
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-07 17:13:45 -0500

    Added main file for binary

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+/// REST frontend for the blockchain, so that it can be used and tested
+
+extern crate rustchain;
+
+fn main() {
+    // TODO write up frontend/REST contracts
+    // Planning to use Pencil
+}
