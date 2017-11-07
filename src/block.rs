8af8afa58b277b40145d22b9e3db39adc1c9362d
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-06 22:20:37 -0500

    Updated block with new function and hash function

--- src/block.rs
+++ src/block.rs
@@ -10,10 +10,40 @@
 /// - proof
 /// - a hash of the previous block
 
+use blockchain::Blockchain;
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
 #[derive(Debug)]
+#[derive(Hash)]
 pub struct Block {
-    index: u64, // index in the blockchain
+    index: usize, // index in the blockchain
     proof: u64,
     prev_hash: String, // hash of the previous block
+    timestamp: Duration,
 }
+
+impl Block {
+    fn new(bchain: &Blockchain, proof: u64, prev_hash: String) -> Block {
+        let sys_time = SystemTime::now();
+        let u_time = sys_time.duration_since(UNIX_EPOCH).expect("Negatively elapsed time");
+
+        Block {
+            index: bchain.get_chain_len() + 1,
+            prev_hash: prev_hash,
+            proof: proof,
+            timestamp: u_time,
+        }
+    }
+}
+
+/// Hashes a block, not placed in struct because blocks can't hash themselves
+/// (needs the previous block)
+fn hash(block: &Block) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    block.hash(&mut hasher);
+    hasher.finish()
+}
+
 
