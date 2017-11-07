33299a5597e0e1da6d2aba6a3b7b1e86ae84cfdf
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-06 22:53:13 -0500

    Implemented hash function for transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -3,9 +3,24 @@
 /// a sender, recipient, and an amount indicating how much the 
 /// transaction was for
 
+use std::hash::{Hash, Hasher};
+
 #[derive(Debug)]
 pub struct Transaction {
     sender: String,
     recipient: String,
     amount: f64,
+}
+
+// Manually implement because float can't be hashed
+impl Hash for Transaction {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.sender.hash(state);
+        self.recipient.hash(state);
+
+        // convert float to string since floating point literals can't be 
+        // hashed themselves (because NaN != NaN)
+        let amount_str = self.amount.to_string();
+        amount_str.hash(state);
+    }
 }
