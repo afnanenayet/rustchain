16fff03dd7276bd3f83cb906b583093b563f2aca
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-06 22:46:08 -0500

    Added function to add a block to the blockchain

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -14,9 +14,14 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
-    
+    /// Returns the length of the blockchain
+    pub fn get_chain_len(&self) -> u64 {
+        self.chain.len() as u64
+    }
+
+    /// Adds a block to the blockchain
+    pub fn add_block(&mut self, block: Block) {
+        self.chain.push(block);
+    }
 }
 
-/// hashes a given block
-fn hash(block: &Block) {
-}
