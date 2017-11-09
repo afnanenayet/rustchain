9a77da17088af4312d542d879535f6e6ef8e9db5
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-09 00:53:02 -0500

    Fixed endpoints/JSON parsing

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,18 @@ extern crate lazy_static;
 use std::sync::Mutex;
 
 use pencil::{
-    Pencil, 
-    Request, 
-    Response, 
+    Pencil,
+    Request,
+    Response,
     PencilResult,
-    PathBound,
-    jsonify,
 };
 
-use rustchain::block::Block;
 use rustchain::blockchain::{Blockchain, proof_of_work};
 use rustchain::transaction::Transaction;
 use std::collections::HashMap;
 
-// Create static blockchain so app can easily access it
+// Create static blockchain as a singleton for easy access in the rest of the
+// app
 lazy_static! {
     static ref BLOCKCHAIN: Mutex<Blockchain> = Mutex::new(Blockchain::new());
 }
@@ -64,23 +62,43 @@ fn mine_endpoint(_: &mut Request) -> PencilResult {
 /// Returns the entire chain as a JSON object
 fn return_full_chain(_: &mut Request) -> PencilResult {
     // Return all blocks in JSON format
-    let blocks = BLOCKCHAIN.lock().unwrap().get_blocks();
-    Ok(Response::from(serde_json::to_string(&blocks).unwrap()))
+    let json_str = BLOCKCHAIN.lock().unwrap().get_json();
+    Ok(Response::from(json_str))
 }
 
 /// Creates a new transaction and adds it to the blockchain
 fn create_new_transact(req: &mut Request) -> PencilResult {
-    //let json = req.get_json().unwrap();
-    Ok(Response::from("Transaction processed"))
+    let json = req.get_json();
+
+    if json.is_none() {
+        Ok(Response::from("Invalid request"))
+    } else {
+        // Convert JSON data into a hashmap
+        let json_data = json.clone().unwrap().to_string();
+        let json_map: HashMap<String, Transaction> = serde_json::from_str(&json_data).unwrap();
+
+        // Create new transaction from JSON data and push it onto the blockchain
+        let new_transaction = json_map["transaction"].clone();
+
+        // Push transaction onto blockchain and respond to user
+        BLOCKCHAIN.lock().unwrap().push_transaction(new_transaction);
+        println!("Added transaction");
+        Ok(Response::from("Transaction processed"))
+    }
 }
 
 fn main() {
-    // Planning to use Pencil
     let mut app = Pencil::new("/web/rustchain");
-    
+
+    // Set port and IP here
+    let ip = "127.0.0.1";
+    let port = "5000";
+    let addr = format!("{}:{}", ip, port);
+    println!("Your app will run at {}", &addr);
+
     // Register endpoints
     app.get("/mine", "mine", mine_endpoint);
     app.put("/transaction/new", "new transaction", create_new_transact);
     app.get("/chain", "chain", return_full_chain);
-    app.run("127.0.0.1:5000");
+    app.run(&addr);
 }
